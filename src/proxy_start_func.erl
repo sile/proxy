994c355a0840b9e3c338990c2cb67c6b1b32d7d5
KOUCHANG <grasshopper.like.ant@gmail.com>
2014-09-24 20:18:40 +0900

    proxy_start_func.erl に spec と comment を記述.

--- src/proxy_start_func.erl
+++ src/proxy_start_func.erl
@@ -1,5 +1,9 @@
+%% @doc 実行する関数を作成するモジュール.
 -module(proxy_start_func).
 
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported API
+%%----------------------------------------------------------------------------------------------------------------------
 -export([
          make_spawn_func/1,
          make_spawn_func/2,
@@ -14,35 +18,87 @@
          start_link/1
         ]).
 
+-export_type([
+              start_func/0,
+              start_func_spawn/0, start_func_start/0,
+
+              function/0, args/0, spawn_options/0,
+
+              %% 以下はspawn_optのoption
+              spawn_option/0, priority_level/0
+             ]).
+
+-type start_func() :: start_func_spawn() | start_func_start(). % FIXME: 処理の内容を把握してから名前を検討.
+
+-type start_func_spawn() :: {spawn, module(), function(), args(), spawn_options()}
+                          | {spawn, fun(), spawn_options()}.
+-type start_func_start() :: {start, module(), function(), args(), time(), start_options()}.
+
+-type function() :: atom().
+-type args() :: [term()].
+-type spawn_options() :: [spawn_option()].
+
+-type spawn_option() :: link
+                      | monitor
+                      | {priority, priority_level()}
+                      | {fullsweep_after, non_neg_integer()}
+                      | {min_heap_size, non_neg_integer()}
+                      | {min_bin_vheap_size, non_neg_integer()}.
+%% @doc spawn_optのoption
+
+-type priority_level() :: low | normal | high | max.
+
+-type time() :: integer() | term(). % 現在不使用.
+-type start_options() :: [term()] | term(). % 現在不使用.
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported Functions
+%%----------------------------------------------------------------------------------------------------------------------
+%% @doc 別プロセスで実行する関数セットを作る.
+-spec make_spawn_func(module(), function(), args()) -> start_func_spawn().
 make_spawn_func(Module, Function, Args) ->
     make_spawn_func(Module, Function, Args, []).
 
+-spec make_spawn_func(module(), function(), args(), spawn_options()) -> start_func_spawn().
 make_spawn_func(Module, Function, Args, Options) ->
     {spawn, Module, Function, Args, Options}.
 
+-spec make_spawn_func(fun()) -> start_func_spawn().
 make_spawn_func(Fun) ->
     make_spawn_func(Fun, []).
 
+-spec make_spawn_func(fun(), spawn_options()) -> start_func_spawn().
 make_spawn_func(Fun, Options) ->
     {spawn, Fun, Options}.
 
+%% @doc サーバを実行するための関数セットを作る.
+-spec make_start_func(module(), function(), args()) -> start_func_start().
 make_start_func(Module, Function, Args) ->
     make_start_func(Module, Function, Args, 5000).
 
+-spec make_start_func(module(), function(), args(), time()) -> start_func_start().
 make_start_func(Module, Function, Args, Time) ->
     make_start_func(Module, Function, Args, Time, []).
 
+-spec make_start_func(module(), function(), args(), time(), start_options()) -> start_func_start().
 make_start_func(Module, Function, Args, Time, Options) ->
     {start, Module, Function, Args, Time, Options}.
 
+%% @doc 引数を取得する.
+-spec get_args(start_func()) -> {ok, args()} | error.
 get_args({spawn, _, _, Args, _})    -> {ok, Args};
 get_args({start, _, _, Args, _, _}) -> {ok, Args};
 get_args(_)                         -> error.
 
+%% @doc 引数をセットする.
+-spec set_args(args(), start_func()) -> start_func().
 set_args(Args, Func = {spawn, _, _, _, _})    -> setelement(4, Func, Args);
 set_args(Args, Func = {start, _, _, _, _, _}) -> setelement(4, Func, Args);
 set_args(_Args, Func)                         -> Func.
 
+%% @doc 関数を開始する.
+-spec start_link(start_func_spawn()) -> {ok, pid() | {pid(), reference()}}
+    ;           (start_func_start()) -> {ok, pid()} | term().
 start_link({spawn, Module, Function, Args, Options}) ->
     {ok, spawn_opt(Module, Function, Args, [link | Options])};
 start_link({spawn, Fun, Options}) ->
